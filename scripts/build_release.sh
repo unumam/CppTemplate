c852c94dd3ded6ce41b686c2873870edf2efa366
Ashot Vardanian <ashvardanian@gmail.com>
2021-01-28 11:35:41 +0400

    Changed the path

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Visualizing the dependencies graph.
 # https://cmake.org/cmake/help/latest/module/CMakeGraphVizOptions.html#module:CMakeGraphVizOptions
 # Debug vs Release:
 # https://stackoverflow.com/a/7725055
 cmake -DCMAKE_BUILD_TYPE=Release . --graphviz="graphviz/all.dot" &&
-make -j4
+    make -j4
